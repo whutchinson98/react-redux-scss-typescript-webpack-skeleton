bee50de65c0598a8b0c8a8945c158a35e923122b
whutchinson98 <willhutchinson2.0@gmail.com>
2021-04-09 10:31:30 -0400

    SCSS added

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
 const path = require('path');
 module.exports = {
-  entry: './src/index.tsx',
+  entry: ['./src/index.tsx', './src/sass/main.scss'],
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
   },
-
   module: {
     rules: [
-      {
-        test: /\.(js|ts)x?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+        {
+            test: /\.scss$/,
+            use: [
+                {
+                loader: 'style-loader',
+                }, {
+                loader: 'css-loader',
+                }, {
+                loader: 'sass-loader',
+            }],
         },
-      },
+        {
+            test: /\.(js|ts)x?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+            },
+        },
     ],
   },
   resolve: {
